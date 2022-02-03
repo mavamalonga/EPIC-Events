367a89fb8baa62295c0fe5b6bac5a713da90e3f9
Alpha <mavamalonga.alpha@gmail.com>
2022-02-03 13:50:40 +0100

    authentication forms

---
+++ epicEvents/authentication/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from authentication import forms
+from django.conf import settings
+
+
+def signup(request):
+	pass
