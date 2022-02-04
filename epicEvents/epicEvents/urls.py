471b346a61542e4252bc122db5d5a9dcc465d331
Alpha <mavamalonga.alpha@gmail.com>
2022-02-04 16:06:21 +0100

    api authentication success

--- epicEvents/epicEvents/urls.py
+++ epicEvents/epicEvents/urls.py
@@ -3,6 +3,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from api import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('api/sign-up/', views.SignUpView.as_view(), name='sign-up'),
+    path('api/login/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token-refresh')
 ]
