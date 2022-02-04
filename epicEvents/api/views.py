a09f01fec08afff4b1adb61129c0032ce9ceee2b
Alpha <mavamalonga.alpha@gmail.com>
2022-02-04 15:36:13 +0100

    signUp

--- epicEvents/api/views.py
+++ epicEvents/api/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from authentication import forms
-from django.conf import settings
+from rest_framework import status
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
+from . import models, serializers
+
+
+class SignUpView(APIView):
+
+	def post(self, request):
+		serializer = serializers.SignUpSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
