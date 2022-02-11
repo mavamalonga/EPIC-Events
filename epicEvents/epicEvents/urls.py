953ae804ed410ec95a2a818b5aa63d60551a6cd7
Alpha <mavamalonga.alpha@gmail.com>
2022-02-11 14:21:20 +0100

    upgrade code

--- epicEvents/epicEvents/urls.py
+++ epicEvents/epicEvents/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 import api.views
-import client.views
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
