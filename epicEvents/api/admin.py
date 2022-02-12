7a8cf7c904e6abd50c22d225b072405ec10b2fe4
Alpha <mavamalonga.alpha@gmail.com>
2022-02-12 16:59:21 +0100

    upgrade models

--- epicEvents/api/admin.py
+++ epicEvents/api/admin.py
@@ -4,10 +4,20 @@ from api.models import User, Client, Event, Contract
 
 
 class UserAdmin(UserAdmin):
-	list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff',
+	list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff',
                 'is_active')
 
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'email', 'company_name', 'sales_contact_id')
+
+class EventAdmin(admin.ModelAdmin):
+	list_display = ('name', 'client_id', 'date', 'support_contact_id')
+
+class ContractAdmin(admin.ModelAdmin):
+	list_display = ('event_id', 'client_id', 'sales_contact_id', 'payment_status')
+
+
 admin.site.register(User, UserAdmin)
-admin.site.register(Client)
-admin.site.register(Event)
-admin.site.register(Contract)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Contract, ContractAdmin)
