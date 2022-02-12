9cac2b4bb88d750754550b65afa06fd1fbff0ee9
Alpha <mavamalonga.alpha@gmail.com>
2022-02-12 15:44:20 +0100

    UserAdmin

--- epicEvents/api/admin.py
+++ epicEvents/api/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from api.models import User, Client, Event, Contract
 
 
-admin.site.register(User)
+class UserAdmin(UserAdmin):
+	list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff',
+                'is_active')
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Client)
 admin.site.register(Event)
 admin.site.register(Contract)
