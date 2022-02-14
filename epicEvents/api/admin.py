1780f6f5d26c1f46d865eae9d73ac5fe7d08466e
Alpha <mavamalonga.alpha@gmail.com>
2022-02-14 15:38:39 +0100

    upgrade code admin.py

--- epicEvents/api/admin.py
+++ epicEvents/api/admin.py
@@ -4,17 +4,17 @@ from api.models import User, Client, Event, Contract
 
 
 class UserAdmin(UserAdmin):
-	list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff',
+	list_display = ('username', 'id', 'email', 'first_name', 'last_name', 'is_staff',
                 'is_active')
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'last_name', 'email', 'company_name', 'sales_contact_id')
+    list_display = ('last_name', 'id', 'email', 'company_name', 'sales_contact_id')
 
 class EventAdmin(admin.ModelAdmin):
-	list_display = ('id', 'name', 'client_id', 'date', 'support_contact_id')
+	list_display = ('name', 'id', 'client_id', 'date', 'support_contact_id')
 
 class ContractAdmin(admin.ModelAdmin):
-	list_display = ('id', 'event_id', 'client_id', 'sales_contact_id', 'payment_status')
+	list_display = ('event_id', 'id', 'client_id', 'sales_contact_id', 'payment_status')
 
 
 admin.site.register(User, UserAdmin)
